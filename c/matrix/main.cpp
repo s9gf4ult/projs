cc47d58f9fc9215bf86c070c45be8f3b5929a823
razor <razor@localhost.(none)>
2010-02-24 16:51:30 +0500

    modified:   Makefile 	modified:   main.cpp 	modified:   matrix.hpp

--- c/matrix/main.cpp
+++ c/matrix/main.cpp
@@ -2,26 +2,32 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-string doubleshow(T *argnum)
+string doubleshow(T argnum)
 {
   char *prev = (char *)calloc(1,256);
-  snprintf(prev, 256, "%2.5f  ", *argnum);
+  snprintf(prev, 256, "%2.5f  ", argnum);
   string returned(prev);
   free(prev);
   return returned;
 }
 
+T mula(T param)
+{
+  return param * sin(param);
+}
+
+
 
 int main(int argc, char **argv) {
-  T doubnull = 0;
-  T doub1 = 1;
-  T mula = 12;
-  Matrix themat(2,2,&doubnull);
-  Matrix the2mat(2,2,&doub1);
+  Matrix themat(2,2, 0);
+  Matrix the2mat(2,2, 1);
   Matrix ones(2);
   cout << themat.mulate(the2mat).show(&doubleshow) << endl;
-  cout << the2mat.mulate(&mula).show(&doubleshow) << endl;
+  cout << the2mat.mulate(19).show(&doubleshow) << endl;
+  cout << ones.show(&doubleshow) << endl;
   cout << the2mat.mulate(ones).show(&doubleshow) << endl;
+  cout << the2mat.map(&mula).show(&doubleshow) << endl;
   return 0;
 }
--- c/matrix/matrix.hpp
+++ c/matrix/matrix.hpp
@@ -3,23 +3,26 @@
 using namespace std;
 
 #define T double
-typedef string ((*stringizer)(T*));
 class Matrix {
 private:
   vector<T> data;
   unsigned int x_size,y_size;
 
 public:
-  Matrix(unsigned int x,unsigned int y, T* empty);
+  typedef string ((*stringizer)(T));
+  typedef T ((*mapper)(T));
+  Matrix(unsigned int x,unsigned int y, T empty);
   Matrix(unsigned int);
-  bool set(unsigned int x,unsigned int y,T* param);
-  bool get(unsigned int x,unsigned int y, T* param);
+  Matrix(unsigned int, unsigned int, vector<T>);
+  void set(unsigned int x,unsigned int y,T param);
+  T get(unsigned int x,unsigned int y);
   unsigned int getxsize();
   unsigned int getysize();
   void resize(unsigned int, unsigned int, T *);
   string show(stringizer functrans);
   Matrix mulate(Matrix);
-  Matrix mulate(T *);
+  Matrix mulate(T);
+  Matrix map(mapper);
 };
   
   

de0bc451c93c0ab799490c3e8cf835103d8a6797
razor <razor@localhost.(none)>
2010-02-24 15:51:02 +0500

    some refactoring

--- c/matrix/matrix.cpp
+++ c/matrix/matrix.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-Matrix::Matrix(unsigned int x,unsigned int y, T* empty)
+Matrix::Matrix(unsigned int x,unsigned int y, T empty)
 {
   x_size=x;
   y_size=y;
   if (x * y) 
-    data.resize((x * y), *empty);
+    data.resize((x * y), empty);
 }
 
 Matrix::Matrix(unsigned int size)
@@ -16,35 +16,40 @@ Matrix::Matrix(unsigned int size)
 #ifdef DEBUG
   cout << "construcor ones" << endl;
 #endif
+  x_size = size;
+  y_size = size;
   if (size) {
     T nullelem = 0;
     data.resize((size * size), nullelem);
-    for (unsigned int passer = 0; passer < size; size++)
+    for (unsigned int passer = 0; passer < size; passer++)
       //(passer + (passer * size))
       data[passer * (size + 1)] = 1;
   }
 }
 
-bool Matrix::set(unsigned int x,unsigned int y,T* param)
+void Matrix::set(unsigned int x,unsigned int y,T param)
 {
-  // try
-  //   {
-		data[x + (y * x_size)] = *param;
-		return true;
-  //   }
-  // catch
-  //   return false;
+  if ((x < x_size) && (y < y_size)) {
+    data[x + (y * x_size)] = param;
+    return;
+  } else {
+#ifdef DEBUG
+    cerr <<"Matrix::set received bad params: x=" << x << "  y=" << y << endl;
+#endif
+    return;
+  }
 }
 
-bool Matrix::get(unsigned int x,unsigned int y, T* param)
+T Matrix::get(unsigned int x,unsigned int y)
 {
-  // try
-  // {
-  *param = data[x + (y * x_size)];
-  return true;
-  // }
-  // catch
-  //  return false;
+  if ((x < x_size) && (y < y_size)) {
+    return data[x + (y * x_size)];
+  } else {
+#ifdef DEBUG
+    cerr << "Matrix::get bad params:x=" << x << "  y=" << y << endl;
+#endif
+    return NULL;
+  }
 }
 
 string Matrix::show(stringizer functrans)
@@ -53,14 +58,14 @@ string Matrix::show(stringizer functrans)
     string resultString;
     for (unsigned int yy=0; yy < y_size; yy++)
       for (unsigned int xx=0; xx < x_size; xx++) {
-        string localres = functrans(&data[xx + (yy * x_size)]);
+        string localres = functrans(data[xx + (yy * x_size)]);
         resultString.append(localres);
         if ((x_size - 1) == xx)
           resultString += "\n";
       }
     return resultString;
   }
-  return NULL;
+  return "Empty";
 }
 
 
@@ -69,38 +74,35 @@ Matrix Matrix::mulate(Matrix mulator)
 #ifdef DEBUG
   cout << "mulate by Matrix" << endl;
 #endif
-  T elemnull = 0;
   if ((x_size * y_size) != 0 && (mulator.x_size * mulator.y_size) != 0)
     if (x_size == mulator.getxsize()) {
-      Matrix resultant(mulator.getxsize(), y_size, &elemnull);
+      Matrix resultant(mulator.getxsize(), y_size, 0);
       for (unsigned int y_passer = 0; y_passer < y_size; y_passer++)
         for (unsigned int x_passer = 0; x_passer < mulator.getxsize(); x_passer++) {
           T summator = 0;
           for (unsigned int middle_passer = 0; middle_passer < x_size; middle_passer++) {
-            T getter = 0;
-            mulator.get(x_passer, middle_passer, &getter);
+            T getter = mulator.get(x_passer, middle_passer);
             summator += (getter * data[middle_passer + (y_passer * x_size)]);
           }
-          resultant.set(x_passer, y_passer, &summator);
+          resultant.set(x_passer, y_passer, summator);
         }
       return resultant;
     }
-  Matrix nullmat(0, 0, &elemnull);
+  Matrix nullmat(0);
   return nullmat;
 }
 
-Matrix Matrix::mulate(T *mulator)
+Matrix Matrix::mulate(T mulator)
 {
 #ifdef DEBUG
   cout << "mulate by T" << endl;
 #endif
   if (x_size * y_size) {
-    T nullel = 0;
-    Matrix result(x_size, y_size, &nullel);
+    Matrix result(x_size, y_size, 0);
     for (unsigned int ypas = 0; ypas < y_size; ypas++)
       for (unsigned int xpas = 0; xpas < x_size; xpas++) {
-        T setter = data[xpas * (ypas * x_size)] * (*mulator);
-        result.set(xpas, ypas, &setter);
+        T setter = data[xpas * (ypas * x_size)] * (mulator);
+        result.set(xpas, ypas, setter);
       }
     return result;
   } else {
@@ -126,12 +128,22 @@ void Matrix::resize(unsigned int newx, unsigned int newy , T *nullelem)
 }
     
 
-					
+Matrix Matrix::map(mapper mapperfunc)
+{
+  if (x_size * y_size) {
+    Matrix result(x_size, y_size, 0);
+    for (unsigned int xx = 0; xx < x_size; xx++)
+      for (unsigned int yy = 0; yy < y_size; yy++) {
+        T rc = data[xx + (yy * x_size)];
+        result.set(xx,yy,mapperfunc(rc));
+      }
+    return result;
+  } else {
+#ifdef DEBUG
+    cerr << "Matrix::map mapping matrix with illegal size x_size=" << x_size << "  y_size=" << y_size << endl;
+#endif
+    return NULL;
+  }
+}
 
-
-          
-
-
-
-
-
+    

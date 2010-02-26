5080be7528f610ef92c9b398d2e4084d53946dfd
razor <razor@localhost.(none)>
2010-02-26 15:09:05 +0500

    main.cpp

--- c/matrix/main.cpp
+++ c/matrix/main.cpp
@@ -20,20 +20,11 @@ string doubleshow(T argnum)
 
 
 int main(int argc, char **argv) {
-  T arr[] = {1, 2, 33,
-             23, 44, 55,
-             55, 32, 99};
-  T arr2[] = {2, 2, 2,
-              3, 3, 3,
-              4, 4, 4};
-  Matrix newone(3,3,arr);
-  Matrix newtwo(3,3,arr2);
   T ara[] = {2, 3,
              3, 2};
   T rar[] = {-2, 14,
              1,  -1};
   cout << Matrix::Matrix(2,2,ara).mulate(Matrix::Matrix(2,2,rar)).show(&doubleshow) << endl;
-  cout << newone.mulate(newtwo).show(&doubleshow) << endl;
                 
   return 0;
 }

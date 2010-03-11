87086855f4ed26fcd24c3abe26cf8d9364348b42
segfault <razor@localhost.(none)>
2010-03-11 17:15:36 +0500

    modified:   getdoble.pl

--- sql/getdoble.pl
+++ sql/getdoble.pl
@@ -1,20 +1,29 @@
 #!/usr/bin/perl
 
-$aa = "SELECT * FROM setable434 WHERE \n";
-for ($i = 1; $i<=15; $i++) {
-    if ($i != 15) {
-        $aa = $aa . "field_" . $i . " is not null and field_" . $i . " <> 0 and ";
+$aa = "SELECT * FROM setable434 t1 WHERE
+EXISTS(SELECT * FROM setable434 t2 WHERE
+t1.idmu <> t2.idmu and t1.rowno = t2.rowno and t1.sdate = t2.sdate and t1.edate = t2.edate and ";
+$beg = 4;
+$end = 6;
+for ($i = $beg; $i <= $end; $i++) {
+    if ($i < $end) {
+        $aa .= "t1.field_${i} = t2.field_${i} and ";
     } else {
-        $aa = $aa . "field_" . $i . " is not null and field_" . $i . " <> 0\n";
+        $aa .= "t1.field_${i} = t2.field_${i} ";
     }
 }
-$aa = $aa . "GROUP BY ";
-for ($i = 1; $i<=15; $i++) {
-    if ( $i != 15) {
-        $aa = $aa . "field_" . $i . ", ";
+$aa .= "and ( ";
+for ($i = $beg; $i <= $end; $i++) {
+    if ($i < $end) {
+        $aa .= "(t2.field_${i} is not null and t2.field_${i} <> 0) or ";
     } else {
-        $aa = $aa . "field_" . $i;
+        $aa .= "(t2.field_${i} is not null and t2.field_${i} <> 0) ";
     }
 }
-$aa .= ", idmu, rowno";
+$aa .= ") ";
+    
+
+
+$aa .= ")";
+
 print "$aa\n";

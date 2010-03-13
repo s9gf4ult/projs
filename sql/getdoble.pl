d34b67b8754088f9bb7c2b399c9151a023568716
razor <razor@localhost.(none)>
2010-03-13 21:56:56 +0500

    getdoble.pl - some modification

--- sql/getdoble.pl
+++ sql/getdoble.pl
@@ -6,18 +6,16 @@ t1.idmu <> t2.idmu and t1.rowno = t2.rowno and t1.sdate = t2.sdate and t1.edate 
 $beg = 4;
 $end = 6;
 for ($i = $beg; $i <= $end; $i++) {
+    $aa .= "t1.field_${i} = t2.field_${i} ";
     if ($i < $end) {
-        $aa .= "t1.field_${i} = t2.field_${i} and ";
-    } else {
-        $aa .= "t1.field_${i} = t2.field_${i} ";
+        $aa .= "and ";
     }
 }
 $aa .= "and ( ";
 for ($i = $beg; $i <= $end; $i++) {
+    $aa .= "(t2.field_${i} is not null and t2.field_${i} <> 0) ";
     if ($i < $end) {
-        $aa .= "(t2.field_${i} is not null and t2.field_${i} <> 0) or ";
-    } else {
-        $aa .= "(t2.field_${i} is not null and t2.field_${i} <> 0) ";
+        $aa .= "or ";
     }
 }
 $aa .= ") ";

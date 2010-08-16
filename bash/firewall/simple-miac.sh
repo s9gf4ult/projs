dd1d4a46b9567ab36834073cdfad3a1eb28b27fb
segfault <razor@calculate.local>
2010-08-16 12:26:46 +0000

    now workable absolutely

--- bash/firewall/simple-miac.sh
+++ bash/firewall/simple-miac.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 MY=192.168.0.9
 ACCEPTED_FOR_PROXY=( "192.168.0.48" )
+PUBLIC_PORTS=( "22" "139" "631" "445" )
 
 protect () {
     eval $@ && return 0 ||
-    (stop ; echo failed: $@ && return 1)
+    (stop ; echo failed: $@ ; exit 1)
 }
 
 
 
 start () {
-    protect iptables -P INPUT DROP
-    protect iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-    for addr in ${ACCEPTED_FOR_PROXY};do
-        protect iptables -A INPUT -s ${addr} -d ${MY} -p tcp --dport 3128 -j ACCEPT
+    iptables -P INPUT DROP
+    iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+    for addr in ${ACCEPTED_FOR_PROXY[*]};do
+        iptables -A INPUT -s ${addr} -d ${MY} -p tcp --dport 3128 -j ACCEPT
     done
-    protect iptables -A INPUT -i lo -s 127.0.0.1 -j ACCEPT
-    protect iptables -A INPUT -i lo -s ${MY} -d ${MY} -j ACCEPT
-    protect iptables -A INPUT -d ${MY} -p tcp --dport 22 -j ACCEPT
+    iptables -A INPUT -i lo -s 127.0.0.1 -j ACCEPT
+    iptables -A INPUT -i lo -s ${MY} -d ${MY} -j ACCEPT
+    for ourport in ${PUBLIC_PORTS[*]};do
+        iptables -A INPUT -d ${MY} -p tcp --dport ${ourport} -j ACCEPT
+    done
 }
 
 stop () {
     iptables -t filter -F
+    iptables -P INPUT ACCEPT
 }
 
 case $1 in
@@ -29,4 +33,7 @@ case $1 in
         start;;
     ("stop")
         stop;;
+    ("restart")
+        stop
+        start;;
 esac

a2c5107d5eba59d8cc95d3eeec4c67c6b8f90994
segfault <razor@calculate.local>
2010-08-16 11:48:32 +0000

    listtle changes

--- bash/firewall/simple-miac.sh
+++ bash/firewall/simple-miac.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 MY=192.168.0.9
+ACCEPTED_FOR_PROXY=( "192.168.0.48" )
 
 
 start () {
     iptables -P INPUT DROP
     iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-    iptables -A INPUT -s 192.168.0.48 -d ${MY} -p tcp --dport 3128 -j ACCEPT
+    for addr in ${ACCEPTED_FOR_PROXY};do
+        iptables -A INPUT -s ${addr} -d ${MY} -p tcp --dport 3128 -j ACCEPT
+    done
     iptables -A INPUT -i lo -s 127.0.0.1 -j ACCEPT
     iptables -A INPUT -i lo -s ${MY} -o ${MY} -j ACCEPT
     iptables -A INPUT -d ${MY} -p tcp --dport 22 -j ACCPET

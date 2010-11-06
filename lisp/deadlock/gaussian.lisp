c3a660a1732b12141f8baa9604267ccac3c6a502
segfault <razor@localhost.(none)>
2010-11-06 18:42:01 +0500

    renamed:    ../gaussian.lisp -> gaussian.lisp

--- lisp/deadlock/gaussian.lisp
+++ lisp/deadlock/gaussian.lisp
@@ -159,3 +159,11 @@
 (defclass strategy () ())
 
 (defgeneric check-strategy (strategy account historydata))
+
+(defmethod check-strategy ((strategy strategy) (account account) (hystory hystory-data))
+  (let ((position (make-instance 'trade-position)))
+    (with-candles hystory candle ()
+                  (if (opened? position)
+                      (try-close-position strategy account hystory position candle)
+                      (try-open-position strategy account hystory position candle)))))
+

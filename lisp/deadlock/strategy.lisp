6a843e20eae47ee9aef8b081663399d9a10d8878
segfault <razor@localhost.(none)>
2010-11-08 00:41:36 +0500

    modified:   lisp/deadlock/strategy.lisp 	modified:   lisp/deadlock/trade-position.lisp

--- lisp/deadlock/strategy.lisp
+++ lisp/deadlock/strategy.lisp
@@ -5,6 +5,8 @@
 (defclass strategy () ())
 
 (defgeneric check-strategy (strategy account historydata))
+(defgeneric try-close-position (strategy account hystory position candle))
+(defgeneric try-open-position (strategy account hystory position candle))
 
 (defmethod check-strategy ((strategy strategy) (account account) (hystory hystory-data))
   (let ((position (make-instance 'trade-position)))
@@ -13,3 +15,8 @@
                       (try-close-position strategy account hystory position candle)
                       (try-open-position strategy account hystory position candle)))))
 
+(defclass everyday (strategy)
+  ())
+
+(defmethod try-open-position ((strategy everyday) (account account) (hystory hystory-data) (position trade-position) (candle candle))
+  (
--- lisp/deadlock/trade-position.lisp
+++ lisp/deadlock/trade-position.lisp
@@ -8,3 +8,16 @@
    (position-type :initform :closed :initarg :position-type :reader position-type :documentation "can be :short or :long :closed")
    (back-stop :initform nil :initarg :back-stop :accessor back-stop)
    (profit-stop :initform nil :initarg :profit-stop :accessor profit-stop)))
+
+(defgeneric opened? (position))
+(defgeneric close-position (pos close-time close-coast))
+
+(defmethod opened? ((position trade-position))
+  (and (open-time position)
+       (open-coast position)
+       (not (eq :closed (position-type position)))))
+
+(defmethod close-position ((pos trade-position) close-time close-coast)
+  (setf (close-time pos) close-time
+        (close-coast pos) close-coast
+        (slot-value pos 'position-type) :closed))

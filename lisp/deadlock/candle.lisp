e5b7a66beb51ee724c75c1a2625edeb151a57f21
segfault <razor@localhost.(none)>
2010-11-09 00:53:29 +0500

    modified:   lisp/deadlock/candle.lisp 	modified:   lisp/deadlock/strategy.lisp

--- lisp/deadlock/candle.lisp
+++ lisp/deadlock/candle.lisp
@@ -7,4 +7,11 @@
    (low :initarg :low :reader candle-low)
    (volume :initarg :volume :reader candle-volume)
    (datetime :initarg :datetime :reader candle-datetime)
-   (type :initarg :type :initform :s1 :reader candle-type :documentation "can be :s1 :m1 :m5 :h1 :d1 :month :year")))
+   (type :reader candle-type)
+   (period :initarg :type :initform :s1 :reader candle-period :documentation "can be :s1 :m1 :m5 :h1 :d1 :month :year")))
+
+(defmethod shared-initialize :after ((obj candle) slot-names &rest initargs &key)
+  (setf (slot-value obj 'type) (cond
+                                 ((> (candle-close obj) (candle-open obj)) :long)
+                                 ((< (candle-close obj) (candle-open obj)) :short)
+                                 (t :empty))))
--- lisp/deadlock/strategy.lisp
+++ lisp/deadlock/strategy.lisp
@@ -16,7 +16,21 @@
                       (try-open-position strategy account hystory position candle)))))
 
 (defclass everyday (strategy)
-  ())
+  ((start-day-period :initform (* 40 60) :initarg :start-day-period :accessor everyday-start-period)))
 
 (defmethod try-open-position ((strategy everyday) (account account) (hystory hystory-data) (position trade-position) (candle candle))
-  (
+  (let ((ctype (candle-type (back-step-candle candle :day 1))))
+    (labels ((try-open-long ()
+               (when (and (> (- (candle-datetime candle) (candle-datetime (start-of-the-period candle :day))) (everyday-start-period strategy)) ; если прошло больше 40 минут с начала дня
+                          (> (candle-high candle) (reduce-candle-values #'candle-high #'max (start-of-the-period candle :day) (back-step-candle candle :s1)))) ; максимальное значение цены текущей свечи больше всех максимальных значений свечей с начала дня
+                 (open-long-position account position candle :backstop (list 'lost-percent 2)))
+                   
+               )
+             (try-open-short ()
+               
+               ))
+      (cond
+        ((eq :long ctype) (try-open-long))
+        ((eq :short ctype) (try-open-short))
+        (t (if (not (try-open-long))
+                  (try-open-short)))))))

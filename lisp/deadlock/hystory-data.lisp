452a3f6d213cfb242c05306673a79dcf848195c2
segfault <razor@calculate.local>
2010-11-15 18:12:14 +0500

    modified:   lisp/deadlock/hystory-data.lisp 	взятие предыдущих свечей из истории работает странно - не правильные 	даты начала свечи при дневной свече и предположительно создает свечу 	на не существующих данных, надо проверить есть ли 06.10.2010 в истории 	фьючей для ртс данные

--- lisp/deadlock/hystory-data.lisp
+++ lisp/deadlock/hystory-data.lisp
@@ -18,12 +18,13 @@
 
 (defclass hystory-data ()
   ((sqlite-handle :initform nil :initarg :sqlite-handle :accessor sqlite-handle)
-   (file-name :initform nil :initarg :file-name :reader file-name)))
+   (file-name :initform nil :initarg :file-name :reader file-name)
+   (candle-period :initform :min :initarg :candle-period :reader hystory-candle-period)))
 
 (defgeneric finalize-hystory (hyst))
 (defgeneric back-step-candle (hystory candle period-type &optional steps))
 (defgeneric make-candle-from-period (hystory datetime period))
-(defgeneric back-step-existing-candle (hystory candle period-type &optioan steps)
+(defgeneric back-step-existing-candle (hystory candle period-type &optional steps)
   (:documentation "вернет предыдущую суцествующую свечу, тобиш в которой проходили сделки(не было выходных проходили сделки и прочее"))
 
 (defmethod shared-initialize :after ((obj hystory-data) slot-names &rest initarts &key)
@@ -34,13 +35,16 @@
   (disconnect (sqlite-handle obj))
   (setf (sqlite-handle obj) nil))
 
-(defmethod back-step-candle ((hystory hystory-data) (candle candle) period-type &optional steps)
-  (if (not (period>= period-type (candle-period candle)))
-      (error "candle period has type ~a but period-type is ~a and it less" (candle-period candle) period-type))
+(defmethod back-step-candle ((hystory hystory-data) (datetime number) period-type &optional steps)
+  (if (not (period>= period-type (hystory-candle-period hystory)))
+      (error "candle period has type ~a but period-type is ~a and it less" (hystory-candle-period hystory) period-type))
   (let* ((steps (or steps
                     1))
-         (back-datetime (datetitime-add-period (candle-datetime candle) (neg-period period-type) :times steps)))
+         (back-datetime (datetime-add-period datetime (neg-period period-type) :times steps)))
     (make-candle-from-period hystory back-datetime period-type)))
+
+(defmethod back-step-candle ((hyst hystory-data) (candle candle) period-type &optional steps)
+  (back-step-candle hyst (candle-datetime candle) period-type steps))
         
 (defmethod make-candle-from-period ((hyst hystory-data) datetime period-type)
   (let ((start (start-of-the-period datetime period-type))
@@ -53,9 +57,14 @@
                (not (member "" (list openco closeco highco lowco))))
           (make-instance 'candle :open openco :close closeco :high highco :low lowco :datetime start :period period-type)))))
 
-(defmethod back-step-existing-candle ((hyst hystory-data) (candle candle) period-type &optioal steps)
+(defmethod back-step-existing-candle ((hyst hystory-data) datetime period-type &optional steps)
   (let ((steps (or steps
                    1))
         last)
-    (loop
-         (
+    (iter (for back-counter from 1)
+          (if (equal 0 steps)
+              (return last)
+              (let ((elt (back-step-candle hyst datetime period-type back-counter)))
+                (if elt
+                    (setf last elt
+                          steps (- steps 1))))))))

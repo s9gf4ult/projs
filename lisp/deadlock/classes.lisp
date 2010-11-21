39931d20f96b99bfafc3dae0d1b39e300901c7f1
segfault <razor@localhost.(none)>
2010-11-21 18:48:02 +0500

    classes.lisp

--- lisp/deadlock/classes.lisp
+++ lisp/deadlock/classes.lisp
@@ -10,7 +10,6 @@
    (instruments :initform nil :initarg :instruments :reader quick-instuments :documentation "список торговых инструментов"))
   (:documentation "сущность надо которой будут проводиться все операции и котороая хранит состояние торгового терминала"))
    
-
 (defclass subaccount()
   ((name :initform "subaccount" :initarg :name :reader subaccount-name)
    (free-money :initform nil :initarg :free-money :reader subaccount-free-money :documentation "свободные средства на счете"))
@@ -22,9 +21,18 @@
   (:documentation "сущность олицетворяющая удержанные средства (ГО при открытии позиции удерживается)"))
 
 (defclass request ()
-  ((direction :initarg :direction :reader request-direction :documentation "must be :long or :short")
-   (instrument :initarg :instrument :reader request-instrument :documentation "ссылка на ценную бумагу")
-   (count :initform 1 :initarg :count :reader request-direction
+  ((direction :initarg :direction :reader request-direction :documentation  "must be `:buy' or `:sell'")
+   (instrument :initarg :instrument :reader request-instrument :documentation "ссылка на ценную бумагу (торговый инструмент)")
+   (count :initform 1 :initarg :count :reader request-count :documentation "количество лотов для покупки")
+   (ttl :initform nil :initarg :ttl :accessor request-ttl :documentation "время жизни заявки, по истечении которого должен вызваться коллбэк")
+   (set-date :initform nil :initarg :set-date :reader request-set-date :documentation "дата выставления заявки")
+   (execution-date :initform nil :reader request-execution-date :documentation "дата исполнения заявки")
+   (overtime-date :initform nil :reader request-overtime-date :documentation "дата просрочки исполнения заявки")
+   (state :initform :awaiting :initarg :state :reader request-state :documentation "can be `:awaiting' `:executed' or `:overdated'")
+   (set-callback :initform nil :initarg :set-callback :reader request-set-callback :documentation "коллбэк на установку заявки в торговую систему")
+   (execute-callback :initform nil :initarg :execute-callback :reader request-execute-callback :documentation "коллбэк на исполнение заявки")
+   (overtime-callback :initform nil :initarg :overtime-callback :reader request-overtime-callback :documentation "коллбэк на просрочку заявки"))
+  (:documentation "заявка на покупку - продажу"))
    
    
    

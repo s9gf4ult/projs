d53bba54bead5be009cb913ee2efd67830c0b8aa
segfault <razor@localhost.(none)>
2011-01-17 02:23:33 +0500

    modified:   lisp/coastcalc.lisp 	micex-compute added

--- lisp/coastcalc.lisp
+++ lisp/coastcalc.lisp
@@ -35,7 +35,7 @@
     `(:backstop ,back-stop-coast
                 :takeprofit-limit ,take-profit-coast)))
 
-(defun universal-deal-compute (direction open &key volume count backstop takeprofit backstop-diff takeprofit-diff (backstop-percent 0.02 backstop-percent?) (takeprofit-percent 0.03 takeprofit-percent?) (takeprofit-from-backstop-difff 1.5))
+(defun universal-deal-compute (direction open &key volume count backstop takeprofit backstop-diff takeprofit-diff (backstop-percent 0.02 backstop-percent?) (takeprofit-percent 0.03 takeprofit-percent?) (takeprofit-from-backstop-difff 1.5) get-lossless (commission :micex commission?) step-back)
   (cond
     (volume
      (setf count (if count
@@ -43,7 +43,7 @@
                      (truncate (/ volume open)))))
     (count
      (setf volume (* count open)))
-    (t (error "universal-argument-map need :count or :volume parameter")))
+    (t (error "universal-deal-compute need :count or :volume parameter")))
   (flet ((compute-backstop ()
            (when (not backstop)
              (when (not backstop-diff)
@@ -80,11 +80,57 @@
            (compute-backstop))))))
   `(:open ,open :count ,count :volume ,volume :backstop ,backstop :takeprofit ,takeprofit))
          
-                                        
-                        
-                                 
-    
-                    
-      
+(defun micex-calculate (open direction &key backstop volume count min max)
+  (declare (type symbol direction)
+           (type number open))
+  (cond
+    ((member direction '(l :l :long :buy)) (setf direction :l))
+    ((member direction '(s :s :short :sell)) (setf direction :s))
+    (t 
+     (error "micex-calculate needs direcion be the member of list (:l :long :buy :s :short :sell)")))
+  (when volume
+    (setf count (if count
+                    (min count (truncate (/ volume open)))
+                    (truncate (/ volume open)))))
+  (when count
+    (setf volume (or volume
+                     (* count open))))
+  (unless (and volume count)
+    (error "you must set volume or count at least"))
   
-        
+  (let* ((backstop-diff (if backstop
+                            (case direction
+                              (:l (/ (- open backstop) count))
+                              (:s (/ (- backstop open) count)))
+                            (let ((safe (/ (* 0.02 volume) count)))
+                              (cond
+                                ((and min max) (min safe
+                                                    (let ((a (- max min)))
+                                                      (if (> a 0) a
+                                                          (error "min = ~a max = ~a this is incorrect" min max)))))
+                                ((and (not min) (not max)) safe)
+                                (t (error "you must set min and max both or no one"))))))
+         (backstop (or backstop
+                       (case direction
+                         (:l (- open backstop-diff))
+                         (:s (+ open backstop-diff)))))
+         (takeprofit-stepback (/ backstop-diff 2))
+         (lossless (let ((c (/ (+ (* 2 0.54)
+                                  (* count open (case direction
+                                                  (:s (- 0.002 1))
+                                                  (:l (+ 0.002 1)))))
+                               (* count (case direction
+                                          (:s (* -1 (+ 1 0.002)))
+                                          (:l (- 1 0.002)))))))
+                     (case direction
+                       (:l (- c open))
+                       (:s (- open c)))))
+         (takeprofit (case direction
+                       (:l (+ open takeprofit-stepback lossless))
+                       (:s (- open takeprofit-stepback lossless)))))
+    `(:open ,(float open) :backstop ,(float backstop) :takeprofit ,(float takeprofit) :takeprofit-stepback ,(float takeprofit-stepback))))
+         
+                                      
+                     
+                                         
+    

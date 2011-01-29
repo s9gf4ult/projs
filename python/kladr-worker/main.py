dc591cb13060629ac056719a4f25537c1f45b5f0
segfault <razor@calculate.local>
2011-01-29 10:23:34 +0500

    modified:   python/kladr-worker/main.py

--- python/kladr-worker/main.py
+++ python/kladr-worker/main.py
@@ -15,13 +15,15 @@ class just_do_it:
         self.main_window = self.glade.get_widget('window')
         self.main_window.connect('destroy', gtk.main_quit)
         self.delete_child = self.glade.get_widget('pbutton_delete_parent')
-        self.delete_child.connect('clicked', self.on_delete)
+        self.delete_child.connect('clicked', self.on_child_delete)
         # self.delete_parent = self.glade.get_widget('pbutton_delete_child')
         # self.commit = self.glade.get_widget('pbutton_commit')
         # self.rollback = self.glade.get_widget('pbutton_rollback')
 
-    def on_delete(button, something):
-        gtk.MessageDialog().show()
+    def on_child_delete(button, something):
+        msg = gtk.MessageDialog()
+        msg.props.text = "hello computer !!"
+        msg.show()
         
     def show(self):
         self.main_window.show()
@@ -29,5 +31,4 @@ class just_do_it:
 
 d = just_do_it()
 d.show()
-print("hello")
 gtk.main()

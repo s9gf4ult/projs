3f752c71ab70efd05a7cac0670eab9412da78f37
segfault <razor@localhost.(none)>
2011-01-29 21:22:08 +0500

    blah blah

--- python/kladr-worker/main.py
+++ python/kladr-worker/main.py
@@ -16,9 +16,30 @@ class just_do_it:
         self.main_window.connect('destroy', gtk.main_quit)
         self.delete_child = self.glade.get_widget('pbutton_delete_parent')
         self.delete_child.connect('clicked', self.on_child_delete)
+        self.delete_parent = self.glade.get_widget('pbutton_delete_child')
+        self.delete_parent.connect('clicked', self.on_parent_delete)
+        self.commit = self.glade.get_widget('pbutton_commit')
+        self.commit.connect('clicked', self.on_commit)
+        self.rollback = self.glade.get_widget('pbutton_rollback')
+        self.rollback.connect('clicked', self.on_rollback)
+        self.open = self.glade.get_widget('pbutton_open')
+        self.open.connect('clicked', self.on_open)
+        
         # self.delete_parent = self.glade.get_widget('pbutton_delete_child')
         # self.commit = self.glade.get_widget('pbutton_commit')
         # self.rollback = self.glade.get_widget('pbutton_rollback')
+
+    def on_open(button, something):
+        pass
+
+    def on_rollback(button, something):
+        pass
+
+    def on_parent_delete(button, something):
+        pass
+
+    def on_commit(button, something):
+        pass
 
     def on_child_delete(button, something):
         msg = gtk.MessageDialog()
@@ -26,8 +47,7 @@ class just_do_it:
         msg.show()
         
     def show(self):
-        self.main_window.show()
-
+        self.main_window.show_all()
 
 d = just_do_it()
 d.show()

b51833cb3967eeec95a4bbc8c88e1bd9e8d1fdc8
segfault <razor@localhost.(none)>
2011-01-30 00:07:04 +0500

    modified:   gui.glade 	modified:   main.py

--- python/kladr-worker/main.py
+++ python/kladr-worker/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-# -*- coding:utf-8 -*-
+# -*- encoding:utf-8 -*-
 
 try:
     import gtk
     import gtk.glade
+    import gobject
 except:
     print("error in loading gtk !!!")
     exit(1)
@@ -17,17 +18,19 @@ class just_do_it:
         self.delete_child = self.glade.get_widget('pbutton_delete_parent')
         self.delete_child.connect('clicked', self.on_child_delete)
         self.delete_parent = self.glade.get_widget('pbutton_delete_child')
-        self.delete_parent.connect('clicked', self.on_parent_delete)
+        self.delete_parent.connect('clicked', self.on_parent_delete, self)
         self.commit = self.glade.get_widget('pbutton_commit')
         self.commit.connect('clicked', self.on_commit)
         self.rollback = self.glade.get_widget('pbutton_rollback')
         self.rollback.connect('clicked', self.on_rollback)
         self.open = self.glade.get_widget('pbutton_open')
         self.open.connect('clicked', self.on_open)
+        self.parent_view = self.glade.get_widget('treeview_parent')
+        self.child_view = self.glade.get_widget('treeview_child')
+        self.parent_list = gtk.ListStore(gobject.TYPE_STRING)
+        self.parent_view.insert_column(gtk.TreeViewColumn(u'hee', gtk.CellRendererText()), -1)
+        self.parent_view.set_model(self.parent_list)
         
-        # self.delete_parent = self.glade.get_widget('pbutton_delete_child')
-        # self.commit = self.glade.get_widget('pbutton_commit')
-        # self.rollback = self.glade.get_widget('pbutton_rollback')
 
     def on_open(button, something):
         pass
@@ -35,16 +38,14 @@ class just_do_it:
     def on_rollback(button, something):
         pass
 
-    def on_parent_delete(button, something):
-        pass
+    def on_parent_delete(button,sf, self):
+        self.parent_list.append(('hehe',))
 
     def on_commit(button, something):
         pass
 
     def on_child_delete(button, something):
-        msg = gtk.MessageDialog()
-        msg.props.text = "hello computer !!"
-        msg.show()
+        pass
         
     def show(self):
         self.main_window.show_all()

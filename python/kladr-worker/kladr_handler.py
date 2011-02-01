a9c056cdf4a35e729b994533a2b243472c7a83ff
segfault <razor@calculate.local>
2011-02-01 14:38:27 +0500

    new file:   kladr_handler.py

---
+++ python/kladr-worker/kladr_handler.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+# -*- coding: utf-8 -*-
+
+
+try:
+    import sqlite3
+except:
+    print("can not load sqlite3")
+
+class kladr_handler:
+    def __init__(self, filename):
+        pass
+
+    def check_database():
+        raise Exception("none")
+
+    def get_parent_list():
+        pass
+
+    def get_child_list():
+        pass
+        

176b9020fb40de23293d14b9a74867d30aca606b
segfault <razor@calculate.local>
2011-03-23 14:54:36 +0500

    ok

--- python/fufu.py
+++ python/fufu.py
@@ -4,7 +4,7 @@ import uuid
 def make_tables(con):
     con.execute("pragma foreign_keys = on")
     con.execute("create table touch (id integer primary key not null, parent integer, val text)")
-    con.execute("create table me (id integer primary key not null, node integer not null, left integer not null, right integer not null, foreign key (node) references touch(id), unique(node), unique(left), unique(right))")
+    con.execute("create table me (id integer primary key not null, node integer not null, left integer not null, right integer not null, foreign key (node) references touch(id) on delete cascade, unique(node), unique(left), unique(right))")
     con.execute("create index left_me on me(left)")
     con.execute("create index right_me on me(right)")
     con.execute("create index node_me on me(node)")
@@ -20,15 +20,16 @@ def remake_hierarhy(con):
     (count,) = con.execute("select count(*) from touch").fetchone()
     (root,) = con.execute("select count(*) from touch where parent is null").fetchone()
     leng = count * 100
-    insert_me_in_range(con, 1, leng, None)
+    insert_me_in_range(con, 0, leng, None)
 
 def insert_me_in_range(con, left, right, parent):
-    (count,) = con.execute("select count(*) from touch where parent = ?", (parent,)).fetchone()
+    (count,) = parent != None and con.execute("select count(*) from touch where parent = ?", (parent,)).fetchone() or con.execute("select count(*) from touch where parent is null").fetchone()
     if count == 0:
         return
     leng = right - left - 2
     onelen = (int(leng) / count) - 1
-    for x in xrange(0, count) and (d,) in con.execute("select id from touch where parent = ?"):
+    cur = parent != None and con.execute("select id from touch where parent = ?", (parent,)) or con.execute("select id from touch where parent is null")
+    for (x, (d,)) in map(lambda a, b: (a, b), xrange(0, count),  cur):
         lf = left + (x * onelen) + 1
         rg = lf + onelen - 1
         con.execute("insert into me(node, left, right) values (?, ?, ?)", (d, lf, rg))
@@ -38,6 +39,12 @@ def insert_me_in_range(con, left, right, parent):
 if __name__ == "__main__":
     con = sqlite3.connect("fufu.sqlite")
     make_tables(con)
-    insert_randoms(con, 3, None, 5)
-    remake_hierarhy(con)
+    con.execute("begin transaction")
+    try:
+        insert_randoms(con, 3, None, 100)
+        remake_hierarhy(con)
+    except:
+        con.rollback()
+    else:
+        con.commit()
     con.close()

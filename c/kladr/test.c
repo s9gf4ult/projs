bf8b53c063c4afb7a99c50403c188d5c26666c59
segfault <razor@localhost.(none)>
2011-06-20 00:18:12 +0600

    modified:   test.c

--- c/kladr/test.c
+++ c/kladr/test.c
@@ -7,10 +7,10 @@ int main(int argc, char **argv) {
   GtkTreeStore *model = gtk_tree_store_new(3, G_TYPE_UINT, G_TYPE_STRING, G_TYPE_STRING);
   GtkTreeIter iter;
 
-  for(guint i=0; i<=10; i++) {
+  for(guint i=0; i<=45; i++) {
     gtk_tree_store_append(model, &iter, NULL);
     gchar *val1 = g_strdup_printf("There is the %d parrots on the wall", i);
-    gchar *val2 = g_strdup_printf("Eat more than %d this franch batoons !!!", i*200);
+    gchar *val2 = g_strdup_printf("Eat over %d this franch batoons !!!", i*200);
     gtk_tree_store_set(model, &iter, 0, i, 1, val1, 2, val2, -1);
     g_free(val1); g_free(val2);
   }

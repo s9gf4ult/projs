1ada53407474e0efc26f5f0fe79f2b27ac723602
segfault <s9gf4ult@gmail.com>
2012-01-25 18:21:26 +0600

    postmodern something

---
+++ lisp/tests/postmodern.lisp
@@ -0,0 +1,14 @@
+(require '#:postmodern)
+
+(defpackage #:pmtest
+  (:use #:cl #:postmodern))
+
+(in-package #:pmtest)
+
+(defclass employee ()
+  ((id :col-type integer :initarg :id :reader object-id)
+   (name :col-type string :initarg :name :accessor employee-name)
+   (email :col-type string :initarg :email :accessor employee-email))
+  (:metaclass dao-class)
+  (:yes id))
+

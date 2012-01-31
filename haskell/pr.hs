cb21d986f856baf201f78e02e9f733b44133b7c1
segfault <s9gf4ult@gmail.com>
2012-02-01 02:55:28 +0600

    modified:   haskell/pr.hs

--- haskell/pr.hs
+++ haskell/pr.hs
@@ -1,23 +1,29 @@
 
-import Text.Printf        
-data Shape = Circle Radius Vertex
-           | Square Vertex Vertex
-           | Triangle Vertex Vertex Vertex
-           | Polygon [Vertex]
-           deriving Show
+data Shape t = Circle t (t, t)
+             | Square (t,t) (t, t)
+             | Triangle (t, t) (t, t) (t, t)
+             | Polygon [(t, t)]
+             deriving Show
 
-type Radius = Float
-type Vertex = (Float, Float)
 
 area (Circle r _) = 3.14 * (r ^ 2 )
 area (Square (x1, y1) (x2, y2)) = abs ((x1 - x2) * (y1 - y2))
 area (Triangle v1 v2 v3) = sqrt(p * (p - a) * (p - b) * (p - c))
-     where distBetween (x1, y1) (x2, y2) = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+     where distBetween (x1, y1) (x2, y2) = sqrt((x1 - x2)^2 + (y1 - y2)^2) 
            a = distBetween v1 v2
            b = distBetween v1 v2
            c = distBetween v2 v3
            p = (a + b + c) / 2
+           
 area (Polygon (v1:v2:v3:vs)) = area (Triangle v1 v2 v3) + area (Polygon (v1:v3:vs))
 area (Polygon (v1:v2)) = 0
 
-main = printf "%f" (area (Polygon [(1, 0), (0.8, 0.2), (0.6, 0.4), (0.2, 0.8), (0, 1), (0, 0)]) :: Float)
+rightTriangles max perimetr = [(a, b, c) | a <- [1..max], b <- [1..a], c <- [1..b], c^2 + b^2 == a^2, a+b+c == perimetr]
+
+concsd d (x:x':xs) = (show x) ++ d ++ (concsd d (x':xs))
+concsd d (x:[]) = (show x)
+
+data Vector a = Vector a a
+     deriving Show
+
+addvec (Vector x1 y1) (Vector x2 y2) = (Vector (x1+x2) (y1+y2))

e95f35f0db0fccc116afa507b6c735f98c3a80db
Alex <s9gf4ult@gmail.com>
2012-02-02 09:08:46 +0600

    modified:   lisp/tests/postmodern.lisp

--- lisp/tests/postmodern.lisp
+++ lisp/tests/postmodern.lisp
@@ -6,7 +6,7 @@
 (in-package #:pmtest)
 
 (defclass employee ()
-  ((id :col-type integer :initarg :id :reader object-id)
+  ((id :col-type integer :primary-key t :default (:nextval blahblah))
    (name :col-type string :initarg :name :accessor employee-name)
    (email :col-type string :initarg :email :accessor employee-email))
   (:metaclass dao-class)

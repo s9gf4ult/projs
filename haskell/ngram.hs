bdfecb7cde70756a38e4e129868676d05c3513b7
segfault <s9gf4ult@gmail.com>
2012-03-16 16:31:26 +0600

    modified:   ngram.hs

--- haskell/ngram.hs
+++ haskell/ngram.hs
@@ -1,12 +1,16 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
-module NGramm where
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances, BangPatterns #-}
+module Main where
 
 import System.Random
+import System.Environment
+import System.IO
 import Control.Arrow ((&&&))
 import Control.Monad
 import Data.List (inits, tails)
 import Data.Text (pack, unpack, replace)
+import Data.Maybe (fromMaybe)
 import qualified Data.Map as M
+import qualified Data.HashTable as T
 import Data.Monoid
 
 -- average :: Fractional a => [a] -> a
@@ -167,22 +171,54 @@ wish       = rule $ "на добрую память" <+> (". " <|> "и с наи
 
 sequence2  = rule $ ("" <|> "желаю ") <+> ("успехов " <|> "удачи ") <+> ("на профессиональном поприще. " <|> "в области программирования. ")
 
+nlists :: Int -> [a] -> [[a]]
+nlists n str = map (take n)
+               $ take (length str + 1 - n)
+               $ tails str
 
 makeNgram :: (Ord a,Num b) => Int -> [a] -> M.Map [a] b
-makeNgram n str = foldr foldf M.empty $ map (take n)
-                  $ take (length str + 1 - n)
-                  $ tails str
+makeNgram n str = foldr foldf M.empty $ nlists n str
   where
     foldf s m = M.insertWith (+) s 1 m
 
-ruleFromNgram :: (AsAtom a) => M.Map a Int -> Rule
-ruleFromNgram m = foldr (<|>) (Rule []) $ map (\(a, b) -> b <:> a) $ M.toList m
+makeHNgram :: (Num a) => Int -> String -> IO (T.HashTable String a)
+makeHNgram n str = foldr foldf (T.new (==) T.hashString) $ nlists n str
+  where
+    foldf st hio = do h <- hio
+                      found <- T.lookup h st
+                      if found == Nothing
+                        then T.insert h st 1
+                        else do T.delete h st
+                                T.insert h st (1 + (fromMaybe 0 found))
+                      return h
+                      
+ruleFromList :: (AsAtom a) => [(a, Int)] -> Rule
+ruleFromList m = foldr (<|>) (Rule []) $ map (\(a, b) -> b <:> a) m
 
 replicateRule :: Int -> Rule -> [Atom]
 replicateRule n r = foldr (<+>) [] $ replicate n r
 
 generateText :: Int -> Int -> String -> IO String
-generateText len amount s = flattenRule (rule $ replicateRule amount $ ruleFromNgram $ makeNgram len s)
+generateText len amount s = flattenRule (rule $ replicateRule amount $ ruleFromList $ M.toList $ makeNgram len s)
+
+generateHText :: Int -> Int -> String -> IO String
+generateHText len amount str = do ngram <- makeHNgram len str
+                                  rulelist <- T.toList ngram
+                                  flattenRule (rule $ replicateRule amount $ ruleFromList $ rulelist)
 
 
 sample m n = generateText m n "Работает же она так. Она выбирает из словаря только те элементы, начало (init) которых совпадает с последними символами генерируемой строки, полученной с предыдущего шага генерации. Размер сравниваемых строк одинаков, так что должно быть точное совпадение по символам. Далее этот отфильтрованный словарь преобразуется в список пар, из которого при помощи рассмотренной ранее функции getRandomElementWithProbabilities выбирается с учётом вероятностей один элемент. Далее из этого элемента берётся последний символ, который и становится той самой «последней буквой», возвращаемой функцией"
+
+main = do
+  a <- getArgs
+  if length a /= 3
+    then usage
+    else do let len = read $ a !! 0
+            let amount = read $ a !! 1
+            cont <- readFile $ a !! 2
+            result <- generateHText len amount cont
+            putStrLn result
+              
+
+usage :: IO ()
+usage = putStrLn "Need 3 arguments :: length amount filename"

7333dc5717d5dbdc31eb83223e43f8e3bee21258
segfault <s9gf4ult@gmail.com>
2012-03-21 14:55:49 +0600

    asdf

---
+++ haskell/ngrams/ngramfilter.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import System.Environment
+import System.IO
+import Control.Monad.Trans.Maybe (runMaybeT)
+import Control.Monad.Trans.Class (lift)
+import Data.Map as M
+import NGram
+  
+
+main = runMaybeT $ do
+  args <- lift $ getArgs
+  if length args /= 3
+    then lift usage
+    else do let len = read $ args !! 0
+            let amount = read $ args !! 1
+            hfile <- lift $ openFile (args !! 2) ReadMode
+            cont <- lift $ hGetContents hfile
+            out <- generateSeq amount $ (makeNgram len cont :: M.Map String Int)
+            lift $ putStrLn out
+            lift $ hClose hfile
+
+usage :: IO ()
+usage = putStrLn "Need 3 arguments length, amount and filename"
+  

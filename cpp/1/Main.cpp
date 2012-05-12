0877314f0a82bd1bb7546f85466f0196b38e2a01
segfault <s9gf4ult@gmail.com>
2012-05-12 16:26:39 +0600

    asdf

--- cpp/1/Main.cpp
+++ cpp/1/Main.cpp
@@ -1,17 +1,59 @@
 #include <iostream>
 #include "Main.hpp"
 
-Sum *Sum::mappend(const Sum *a, const Sum *b) {
-  Sum *ret = new Sum(a->elt + b->elt);
-  return ret;
+void ConcreteVisitor1::visit(ConcreteElement1 *elt) {
+  std::cout << "visitor 1 visit element 1" << std::endl;
+}
+
+void ConcreteVisitor1::visit(ConcreteElement2 *elt) {
+  std::cout << "vistor 1 visit element 2" << std::endl;
+}
+
+void ConcreteVisitor2::visit(ConcreteElement1 *elt) {
+  std::cout << "visior 2 visit element 1" << std::endl;
+}
+
+void ConcreteVisitor2::visit(ConcreteElement2 *elt) {
+  std::cout << "visitor 2 visit element 2" << std::endl;
+};
+
+void ConcreteElement1::accept(Visitor *vst) {
+  vst->visit(this);
+}
+
+void ConcreteElement2::accept(Visitor *vst) {
+  vst->visit(this);
+}
+
+void ConcreteVisitor3::visit(ConcreteElement3 *elt) {
+  std::cout << "visitor 3 visit element 3" << std::endl;
+}
+
+void ConcreteElement3::accept(Visitor *vst) {
+  vst->visit(this);
+}
+
+void ConcreteElement3::accept(ConcreteVisitor3 *vst) {
+  vst->visit(this);
 }
 
 
 int main(int argc, char **argv) {
-  Monoid *s = new Sum(10);
-  Monoid *b = new Sum(20);
-  
-  
-  std::cout << "hello";
-  
+
+  Visitor *vs1 = new ConcreteVisitor1();
+  Visitor *vs2 = new ConcreteVisitor2();
+  ConcreteVisitor3 *vs3 = new ConcreteVisitor3();
+
+  ConcreteElement1 *elt1 = new ConcreteElement1();
+  ConcreteElement2 *elt2 = new ConcreteElement2();
+  ConcreteElement3 *elt3 = new ConcreteElement3();
+
+  elt1->accept(vs1);
+  elt1->accept(vs2);
+  elt2->accept(vs1);
+  elt2->accept(vs2);
+  elt3->accept(vs3);
+  elt1->accept(vs3);
+
+  delete vs1, vs2, vs3, elt1, elt2, elt3;
 }
--- cpp/1/Main.hpp
+++ cpp/1/Main.hpp
@@ -1,20 +1,59 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-class Monoid {
+class Visitor;
+class ConcreteVisitor1;
+class ConcreteVisitor2;
+class ConcreteVisitor3;
+class Element;
+class ConcreteElement1;
+class ConcreteElement2;
+class ConcreteElement3;
+
+class Visitor {
 public:
-  virtual Monoid *mappend(const Monoid *a, const Monoid *b) = 0;
+  virtual void visit(ConcreteElement1 *elt) = 0;
+  virtual void visit(ConcreteElement2 *elt) = 0;
 };
 
-class Sum : public Monoid {
-  float elt;
+class ConcreteVisitor1 : public Visitor {
 public:
-  Sum(float elt): elt(elt) {
-  };
+  virtual void visit(ConcreteElement1 *elt);
+  virtual void visit(ConcreteElement2 *elt);
+};
 
-  virtual Sum *mappend(const Sum *a, const Sum *b);
-};  
+class ConcreteVisitor2 : public Visitor {
+public:
+  virtual void visit(ConcreteElement1 *elt);
+  virtual void visit(ConcreteElement2 *elt);
+};
 
+class ConcreteVisitor3 : public ConcreteVisitor2{
+public:
+  virtual void visit(ConcreteElement2 *elt);
+  virtual void visit(ConcreteElement3 *elt);
+};
+
+class Element {
+public:
+  virtual void accept(Visitor *vst) = 0;
+};
+  
+class ConcreteElement1 : public Element {
+public:
+  virtual void accept(Visitor *vst);
+};
+
+class ConcreteElement2 : public Element {
+public:
+  virtual void accept(Visitor *vst);
+};
+
+class ConcreteElement3 : public Element {
+public:
+  virtual void accept(Visitor *vst);
+  virtual void accept(ConcreteVisitor3 *vst);
+};
   
 
 #endif

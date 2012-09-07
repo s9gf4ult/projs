38c2a71bc69cc4f7e082807d538142cb96a44a5b
segfault <s9gf4ult@gmail.com>
2012-09-07 17:51:41 +0600

    modified:   heist.hs

--- haskell/heist/heist.hs
+++ haskell/heist/heist.hs
@@ -3,7 +3,10 @@
 import Blaze.ByteString.Builder
 import Control.Monad.Trans
 import Control.Monad.Trans.Error
+import Data.ByteString.Char8 as B
 import Data.Text as T
+import Data.Text.IO as TIO
+import Prelude as P
 import Text.Templating.Heist
 import Text.XmlHtml as X
 
@@ -13,9 +16,9 @@ main = do
              $ bindSplices [("mySplice", mySplice)]  defaultHeistState
     tmpl <- lift $ renderTemplate state "default"
     maybe (throwError "Could not render template") (return . toByteString . fst) tmpl
-  either putStrLn print a
+  either P.putStrLn B.putStrLn a
 
 mySplice :: (Monad m, MonadIO m) => Splice m
 mySplice = do
-  s <- liftIO getLine 
-  return [Element "div" [] [TextNode $ pack s]]
+  s <- liftIO TIO.getLine 
+  return [Element "div" [] [TextNode s]]

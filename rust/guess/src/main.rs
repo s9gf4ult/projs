ff31fd94fc1273b85d90f96b3fb82a82d50a3836
Aleksey Uimanov <s9gf4ult@gmail.com>
2017-09-16 14:31:04 +0500

    fuck

--- rust/guess/src/main.rs
+++ rust/guess/src/main.rs
@@ -1,32 +1,22 @@
 extern crate rand;
 
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+// use std::io;
+// use std::cmp::Ordering;
+// use rand::Rng;
+
+// fn take_str(s : & String) -> String {
+//     let mut ret = s.clone();
+//     ret.push_str("much hello");
+//     ret
+// }
 
 fn main() {
-    println!("Enter some shit");
-    let r : u32 = rand::thread_rng().gen_range(1,101);
-    loop {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).expect("fuck");
-        let num : u32 = match s.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Wrong numger, reenter");
-                continue;
-            }
-        };
-        match num.cmp(& r) {
-            Ordering::Less =>
-                println!("too small"),
-            Ordering::Greater =>
-                println!("too big"),
-            Ordering::Equal => {
-                println!("guessed!");
-                break;
-            },
-        }
-    }
+    let s = String::from("жопа");
+    let t : String = s.chars().skip(3).collect();
+    println!("{}", s);
+    println!("{}", t);
 }
+
+// thread 'main' panicked at 'byte index 3 is not a char boundary; it is inside 'о' (bytes 2..4) of `жопа`', src/libcore/str/mod.rs:1771
+// note: Run with `RUST_BACKTRACE=1` for a backtrace.

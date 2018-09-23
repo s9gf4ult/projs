f24a6a818dc2313c51502f7ff09df7baee900687
Aleksey Uimanov <s9gf4ult@gmail.com>
2018-09-23 21:29:25 +0500

    fix

--- haskell/mu-reader/src/Lib.hs
+++ haskell/mu-reader/src/Lib.hs
@@ -1,29 +1,55 @@
 module Lib where
 
+import Control.Lens
 import Control.Monad.Base
 import Control.Monad.Reader
+import Data.Generics.Product
+import GHC.Generics (Generic)
 
-newtype Mu f = Mu (f (Mu f))
+data TruthService m = TruthService
+  { truth :: m Int
+  } deriving (Generic)
 
+truthService :: (MonadBase IO m) => TruthService m
+truthService = TruthService
+  (liftBase (putStrLn "Calculating truth") *> return 42)
 
-data Struct m = Struct
-  { theTruth :: m Int
+lieService :: (Monad m) => TruthService m
+lieService = TruthService (return 666)
+
+data MultipleService m = MultipleService
+  { truth :: m Int
+  , currentUser :: m Int
+  } deriving (Generic)
+
+multipleService :: (MonadBase IO m) => MultipleService m
+multipleService = MultipleService
+  { truth = liftBase (putStrLn "Haha !") *> return 55
+  , currentUser = return 100
   }
 
-struct :: (MonadBase IO m) => Struct m
-struct = Struct
-  (liftBase (print 43) *> return 42)
-
-useStruct :: (MonadBase IO m, MonadReader (Struct m) m) => m Int
-useStruct = do
-  liftBase $ print "Not the trueth appears"
+useTruthService
+  :: forall m r
+  . (MonadBase IO m, MonadReader r m, HasField' "truth" r (m Int))
+  => m ()
+useTruthService = do
+  liftBase $ putStrLn "Now the truth appears"
   s <- ask
-  theTruth s
+  t <- s ^. field' @"truth"
+  liftBase $ print t
 
+newtype SelfReader s m a = SelfReader (ReaderT (s (SelfReader s m)) m a)
+  deriving (Functor, Applicative, Monad)
 
-newtype SelfReader a m = SelfReader (ReaderT (Struct m) IO a)
+deriving instance (Monad m, MonadBase m m) => MonadBase m (SelfReader s m)
 
-type MuReader a = Mu (SelfReader a)
+deriving instance (Monad m) => MonadReader (s (SelfReader s m)) (SelfReader s m)
 
-runStructReader :: Struct m -> ReaderT (Struct m) a -> IO a
-runStructReader = error "FIXME: runStructReader not implemented"
+runSelfReader :: s (SelfReader s m) -> SelfReader s m a -> m a
+runSelfReader struct (SelfReader r) = runReaderT r struct
+
+test :: IO ()
+test = do
+  runSelfReader truthService useTruthService
+  runSelfReader lieService useTruthService
+  runSelfReader multipleService useTruthService

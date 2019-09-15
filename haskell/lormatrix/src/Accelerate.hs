9c5c07519b7a104ca6f5ac1450bb82c4ac097650
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-09-15 22:09:05 +0500

    shit

--- haskell/lormatrix/src/Accelerate.hs
+++ haskell/lormatrix/src/Accelerate.hs
@@ -1,6 +1,22 @@
 module Main where
 
+import           Data.Array.Accelerate
+import qualified Prelude               as P
 
+multiplyMatrixMatrix
+  :: (Num a)
+  => Acc (Matrix a)
+  -> Acc (Matrix a)
+  -> Acc (Matrix a)
+multiplyMatrixMatrix x y =
+  let
+    Z :. rows :. _cols  = unlift ((shape x) :: Exp DIM2 )
+    Z :. _rows :. cols  = unlift ((shape y) :: Exp DIM2 )
+  in
+    fold1 (+) $ transpose $
+         zipWith (*)
+            (replicate (lift $ Any :. All :. All :. cols) x)
+            (replicate (lift $ Any :. rows :. All :. All) y)
 
-main :: IO ()
+main :: P.IO ()
 main = error "FIXME: main not implemented"

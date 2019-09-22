ee8411f42f1b9fc5304e7f45b5c71e0968319e52
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-09-22 22:27:33 +0500

    accelerate

--- haskell/lormatrix/src/Accelerate.hs
+++ haskell/lormatrix/src/Accelerate.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import Control.Lens
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Control.Lens (_3, _2)
 import Data.Array.Accelerate.Control.Lens ()
 import           Data.Array.Accelerate
-import           Prelude               (($))
+import           Prelude               (($), (>>=), pure, read)
+import Data.Array.Accelerate.LLVM.Native (run)
 import qualified Prelude               as P
 
 multiplyMatrixMatrix
@@ -21,7 +24,33 @@ multiplyMatrixMatrix x y =
             ((replicate (lift $ Any :. All :. All :. cols) x) :: Acc (Array DIM3 a))
             ((replicate (lift $ Any :. rows :. All :. All) y) :: Acc (Array DIM3 a))
 
+newMatrix :: Int -> Matrix Double
+newMatrix n =
+  let
+    tmp = 1 / P.fromIntegral n / P.fromIntegral n
+  in fromFunction (Z :. n :. n) $ \(Z :. i :. j) ->
+    tmp * P.fromIntegral (i - j) * P.fromIntegral (i + j)
 
 
 main :: P.IO ()
-main = error "FIXME: main not implemented"
+main = do
+  [n] <- getArgs >>= \case
+    [a] -> pure ([read a :: Int])
+    _ -> exitFailure
+
+  -- t1 <- clock
+  let
+    a = newMatrix n
+    b = newMatrix n
+    c = multiplyMatrixMatrix (use a) (use b)
+    center = lift $ n `div` 2
+    res = run $ unit $ c ! (index2 center center)
+  P.print res
+  -- printf "% 8.6f\n" (
+
+  -- t2 <- clock
+  -- printf "%ds\n" $ (t2 - t1) `div` 1000000000
+
+
+-- clock :: P.IO P.Integer
+-- clock = toNanoSecs <$> getTime ProcessCPUTime

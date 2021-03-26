270e9227bc44ade9403758702f12777bc9ac56f9
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-03-26 18:34:57 +0500

    fix

--- rust/book/structs/src/main.rs
+++ rust/book/structs/src/main.rs
@@ -1,3 +1,24 @@
+struct User {
+    name: String,
+    email: String,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut  user = User {
+        name: String::from("tntntn"),
+        email: String::from("ntnt@snsn.com"),
+        active: true,
+    } ;
+
+    println!("{}", &user.name);
+    user.name = String::from("tnaisht");
+    println!("{}", &user.name);
+
+    let user2 = User {
+        name: String::from("ururur"),
+        ..user
+    };
+
+    println!("{}", user.email);
 }

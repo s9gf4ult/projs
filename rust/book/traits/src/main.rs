9e187aa4f8a77be487a848eb707c701fb50a0a0c
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-03-28 20:16:54 +0500

    fix

--- rust/book/traits/src/main.rs
+++ rust/book/traits/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let v1 = vec![1, 2, 50, 10] ;
+    let v2 = vec![1., 22.2, 55.2, 29292.] ;
+    let l1 = largest(&v1) ;
+    println!("{:?}", l1);
+    let l2 = largest(&v2) ;
+    println!("{:?}", l2);
+}
+
+fn largest<T> (l : &[T]) -> Option<&T>
+where T : PartialOrd
+{
+    let mut iter = l.into_iter() ;
+    let mut lrg = iter.next()? ;
+    for item in iter {
+        if item > lrg {
+            lrg = item
+        }
+    } ;
+    Some(lrg)
 }

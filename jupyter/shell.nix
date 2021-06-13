c3af6137084bbb8bc6a84728afa88b73cabfa9a5
Aleksey Uymanov <s9gf4ult@gmail.com>
2021-06-13 23:11:16 +0500

    wip

--- jupyter/shell.nix
+++ jupyter/shell.nix
@@ -1,21 +1,29 @@
 { sources ? import ./nix/sources.nix }:     # import the sources
 let
-  pkgs = import sources.nixpkgs {} ;
-  my-python = python-packages: with python-packages;
-   [ pandas matplotlib seaborn ipywidgets widgetsnbextension ] ;
-in pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    jupyter
-    (python3.withPackages my-python)
-    ruby
-  ] ;
 
-  shellHook = ''
-    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
-    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
-    export PIP_PREFIX=$(pwd)/_build/pip_packages
-    export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
-    export PATH="$PIP_PREFIX/bin:$PATH"
-    unset SOURCE_DATE_EPOCH
-  '';
-}
+  # jupyter = import sources.jupyterWith {} ;
+  # # pkgs = import sources.nixpkgs {} ;
+
+  jupyterLibPath = /home/razor/work/jupyterWith ;
+  jupyter = import jupyterLibPath {};
+
+
+  iPythonWithPackages = jupyter.kernels.iPythonWith {
+      name = "python";
+      packages = p: with p; [
+            numpy
+            scipy
+            pandas
+            matplotlib
+            seaborn
+            umap-learn
+            scikitlearn
+            ];
+      };
+
+  jupyterlabWithKernels = jupyter.jupyterlabWith {
+      kernels = [ iPythonWithPackages ];
+  };
+
+
+in jupyterlabWithKernels.env

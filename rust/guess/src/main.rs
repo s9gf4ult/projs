05845c8aa3531eb15f9159a9f7c7a5699199a76f
Aleksey Uymanov <s9gf4ult@gmail.com>
2023-09-09 16:03:35 +0500

    tntn

--- rust/guess/src/main.rs
+++ rust/guess/src/main.rs
@@ -1,28 +1,43 @@
 use std::{
     cell::RefCell,
-    rc::Rc
+    ops::Deref,
+    rc::Rc,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+    time::Duration,
 };
 
-
 #[derive(Debug)]
-enum List {
-    Cons(i32, RefCell<Rc<List>>),
-    Nil,
+struct T {
+    x: i32,
+    y: i32,
 }
 
-use List::*;
-
-impl List {
-    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
-        match self {
-            Cons(_, item) => Some(item),
-            Nil => None,
-        }
-    }
+fn f() -> i32 {
+    117
 }
 
 fn main() {
-    let r:RefCell<i32> = RefCell::new(10);
-    println!("{}", r.borrow());
-    println!("{}", r.borrow_mut());
+    let a = T { x: 32, y: 64 };
+    match a {
+        T { x: x @ 1..=5, .. } => (),
+        T {
+            x: x @ 1..=10,
+            y: y @ (3 | 7 | 17),
+        } if y > x => (),
+        T {
+            x: x @ (11 | 13 | 17),
+            ..
+        } => println!("{}", x),
+        // T { x: if x > 20, .. } => println!("hehe"),
+        _ => println!("other"),
+    }
+    match f() {
+        1 => println!("odin"),
+        2 => println!("dva"),
+        3..=10 => println!("mnoga"),
+        a @ 11..=100 => println!("oche mnoga {}", a),
+        a @ (111 | 113 | 117) => println!("krasivo {}", a),
+        _ => println!("ohuel?"),
+    }
 }

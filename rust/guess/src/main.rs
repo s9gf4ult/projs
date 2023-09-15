ac9e58628d701469e441d2b8bf8960ccd68d1634
Aleksey Uymanov <s9gf4ult@gmail.com>
2023-09-15 19:15:36 +0500

    fix

--- rust/guess/src/main.rs
+++ rust/guess/src/main.rs
@@ -1,3 +1,6 @@
+extern crate serde;
+extern crate serde_json;
+
 use std::{
     cell::RefCell,
     ops::Deref,
@@ -7,37 +10,41 @@ use std::{
     time::Duration,
 };
 
-#[derive(Debug)]
-struct T {
-    x: i32,
-    y: i32,
+mod ser;
+
+trait Pilot {
+    fn fly(&self);
 }
 
-fn f() -> i32 {
-    117
+trait Wizard {
+    fn fly(&self);
+}
+
+trait X: Pilot + Wizard {}
+
+#[derive(Debug, Clone, Copy)]
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self, s: &str) {
+        println!("*waving arms furiously {}*", s);
+    }
 }
 
 fn main() {
-    let a = T { x: 32, y: 64 };
-    match a {
-        T { x: x @ 1..=5, .. } => (),
-        T {
-            x: x @ 1..=10,
-            y: y @ (3 | 7 | 17),
-        } if y > x => (),
-        T {
-            x: x @ (11 | 13 | 17),
-            ..
-        } => println!("{}", x),
-        // T { x: if x > 20, .. } => println!("hehe"),
-        _ => println!("other"),
-    }
-    match f() {
-        1 => println!("odin"),
-        2 => println!("dva"),
-        3..=10 => println!("mnoga"),
-        a @ 11..=100 => println!("oche mnoga {}", a),
-        a @ (111 | 113 | 117) => println!("krasivo {}", a),
-        _ => println!("ohuel?"),
-    }
+    let p = Human;
+    // p.fly();
+    p.fly("hzhuh");
 }

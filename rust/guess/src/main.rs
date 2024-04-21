38922ff5dbec7556d66f468576ed154381383d89
Aleksey Uymanov <s9gf4ult@gmail.com>
2024-04-21 12:39:01 +0500

    fix

--- rust/guess/src/main.rs
+++ rust/guess/src/main.rs
@@ -4,15 +4,16 @@
 // use std::mem;
 // use std::ops::{Deref, DerefMut};
 
-fn main() {
-    let v: Vec<i32> = vec![1, 2, 3, 4];
-    let mut t = v.iter();
-    let s: i32 = t.by_ref().take(3).cloned().sum();
-    println!("{s}");
-    for i in t {
-        println!("{}", i);
-    }
-}
+// fn main() {
+//     let c = || {};
+//     let v: Vec<i32> = vec![1, 2, 3, 4];
+//     let mut t = v.iter();
+//     let s: i32 = t.by_ref().take(3).cloned().sum();
+//     println!("{s}");
+//     for i in t {
+//         println!("{}", i);
+//     }
+// }
 
 // fn get_default<'m, 'v, K, V>(map: &'m mut HashMap<K, V>, key: K) -> &'v mut V
 // where
@@ -27,3 +28,27 @@ fn main() {
 //         map.get_mut(&key).unwrap()
 //     }
 // }
+
+struct T {
+    x: i32,
+    y: i32,
+}
+
+fn match_t(t: T) {
+    match t {
+        T {
+            x: x_val @ 1..=10,
+            y: y_val @ (3 | 7 | 17),
+        } if y_val > x_val => {
+            println!("Matched: x = {}, y = {}", x_val, y_val);
+        }
+        _ => {
+            println!("Didn't match");
+        }
+    }
+}
+
+fn main() {
+    let t = T { x: 5, y: 7 };
+    match_t(t);
+}
